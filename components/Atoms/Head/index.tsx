23d34f4f2c2adf1ffc00a3947bde970123eadb47
Lucas Fernando <lucasfernando.dev@gmail.com>
2021-08-12 03:45:46 -0300

    fix: resolving thumbnail image erros in meta-tags

--- components/Atoms/Head/index.tsx
+++ components/Atoms/Head/index.tsx
@@ -3,11 +3,10 @@ import { HOME_OG_IMAGE_URL, BASE_URL } from "../../../lib/constants";
 
 type HeadProp = {
   title: string;
-  ogImage?: string | undefined;
+  ogImage?: string;
 };
 
 const Head = ({ title, ogImage }: HeadProp) => {
-  const OG_Thumbnail = ogImage ? ogImage : HOME_OG_IMAGE_URL;
   return (
     <NextHead>
       {title ? (
@@ -16,12 +15,10 @@ const Head = ({ title, ogImage }: HeadProp) => {
         <title>MAGE CSS - Dicas de front-end</title>
       )}
 
-  if(!ogImage === 'undefined'){}
-
-      {OG_Thumbnail !== "undefined" ? (
+      {ogImage ? (
         <meta property="og:image" content={`${BASE_URL}${ogImage}`} />
       ) : (
-        <meta property="og:image" content={OG_Thumbnail} />
+        <meta property="og:image" content={`${HOME_OG_IMAGE_URL}`} />
       )}
     </NextHead>
   );
